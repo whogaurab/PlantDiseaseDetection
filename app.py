f9c75ca00c260066ed417f25fcefccc6c63d26ff
whogaurab <gaurabchoudhary2002@gmail.com>
2024-07-10 14:15:35 +0530

    data ingestion dir added

---
+++ app.py
@@ -0,0 +1,4 @@
+from plantdiseasedetection.pipeline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
---
+++ plantdiseasedetection/constant/training_pipeline/__init__.py
@@ -0,0 +1,34 @@
+ARTIFACTS_DIR: str = "artifacts"
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1EFvLbg0y68SRrXNCeiUFB35B2Xku72QK/view?usp=drive_link"
+
+
+# """
+# Data Validation realted contant start with DATA_VALIDATION VAR NAME
+# """
+
+# DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+# DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+# DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "data.yaml"]
+
+
+
+# """
+# MODEL TRAINER related constant start with MODEL_TRAINER var name
+# """
+# MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+
+# MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov9-c.pt"
+
+# MODEL_TRAINER_NO_EPOCHS: int = 1
+
+# MODEL_TRAINER_BATCH_SIZE: int = 16
